fbdce8acb2f964bbcd8ce701b1ba4b7176ddc6c1
Giacomo Aloisi <giacomo.aloisi@local.ittcesena.it>
2017-01-14 10:59:17 +0100

    Proprety changed initialized

---
+++ KMeans/Coordinates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace KMeans
+{
+    class Coordinates : List<double>, INotifyPropertyChanged
+    {
+        public Coordinates() : base() { }
+
+        public new double this[int i]
+        {
+            get
+            {
+                return base[i];
+            }
+            set
+            {
+                base[i] = value;
+                PropretyChanged();
+            }
+        }
+
+        private void PropretyChanged()
+        {
+            throw new NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
